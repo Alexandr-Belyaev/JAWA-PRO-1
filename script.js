9e13de3ab6a8fdf8f3c21f7ae3e11d17663545c2
Alexandr-Belyaev <113306374+Alexandr-Belyaev@users.noreply.github.com>
2023-09-27 19:28:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+//Задание 1
+const arr = [1, 5, 7, 9];
+console.log(Math.min(...arr));
+console.log("");
+
+// Задание 2
+const createCounter = () => {
+   let count = 0;
+   return {
+      increment: () => ++count,
+      decrement: () => --count,
+   }
+}
+const counter1 = createCounter();
+console.log(counter1.increment());
+console.log(counter1.increment());
+console.log(counter1.increment());
+
+const counter2 = createCounter();
+console.log(counter2.decrement());
+console.log(counter2.decrement());
+console.log(counter2.decrement());
+
+console.log('');
+// Задание 3
+function findElementByClass(rootEl, classEl) {
+   for (let index = 0; index < rootEl.childElementCount; index++) {
+      rootEl = rootEl.children[index];
+      if (rootEl.className === classEl) {
+         return rootEl;
+      }
+   }
+   return findElementByClass(rootEl, classEl);
+}
+
+const rootElement = document.getElementById('root');
+const targetElement = findElementByClass(rootElement, 'my-class');
+console.log(targetElement);
+
+const rootElement_1 = document.getElementById('root1');
+const targetElement_1 = findElementByClass(rootElement_1, 'class4');
+console.log(targetElement_1);
